dc3b235978981125c1e08a016c1f4cf13f204987
KhaledLakehal <khaled.lakehal93@gmail.com>
2021-07-05 19:55:42 -0400

    update files

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 var nameInputEl = document.querySelector('#cityname');
 var userFormEl = document.querySelector('#city-form');
-var repoContainerEl = document.querySelector('#weathers-container');
+var weatherContainerEl = document.querySelector('#weathers-container');
 var cityEl = document.querySelector("#city")
-
+var weatherCityEl = document.querySelector("#cityOfWeather")
+var container1El = document.querySelector("#container1")
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
-    var cityname = nameInputEl.value.trim();
+    var cityname = nameInputEl.value.trim().toUpperCase();
     var cities = JSON.parse(localStorage.getItem("citiesList"))  || [] 
     cities.push(cityname)
     localStorage.setItem("citiesList", JSON.stringify(cities))
    
     if (cityname) {
       getLatlon(cityname);
+      addButton(cityname);
+      weatherCityEl.textContent = cityname
       
-      repoContainerEl.textContent = '';
+      
+    
+      weatherContainerEl.textContent = '';
       nameInputEl.value = '';
     } else {
       alert('Please enter a GitHub username');
@@ -45,7 +50,7 @@ var formSubmitHandler = function (event) {
       });
   };
   var getWeather = function (lat,lon) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&units=imperial&exclude=hourly,minutely&appid=1de90d827e463b75865e4df8ff41370b";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon +"&units=metric&exclude=hourly,minutely&appid=1de90d827e463b75865e4df8ff41370b";
     
     fetch(apiUrl)
       .then(function (response) {
@@ -53,22 +58,11 @@ var formSubmitHandler = function (event) {
           console.log(response);
           response.json().then(function (data) {
             console.log(data.daily); 
-            weatherWeek = data.daily;
-            for(i=0;i<7;i++){
-              
-              weatherDay = weatherWeek[i]
-              day = weatherDay.dt
-              temp = weatherDay.temp.day
-              wind = weatherDay.wind_speed
-              humidity =weatherDay.humidity
-              uvi = weatherDay.uvi
-              console.log(temp,wind,humidity,uvi);
-              console.log(weatherDay);
-              
-            }
-            dateTo = moment(new Date()).format('YYYY-MM-DD');
-            dateFrom = moment(new Date() - 7).format('YYYY-MM-DD')
-            console.log(dateTo);
+            
+             weatherWeek = data.daily;
+             displayWeather(weatherWeek[1],weatherContainerEl)
+             displayWeather(weatherWeek[2],container1El)
+             
             // displayRepos(data, city);
           });
         } else {
@@ -79,8 +73,84 @@ var formSubmitHandler = function (event) {
         alert('Unable to connect to GitHub');
       });
   };
-  userFormEl.addEventListener('submit', formSubmitHandler);
+ 
 
 
    
     
+  function timeConverter(UNIX_timestamp){
+    var a = new Date(UNIX_timestamp * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var time = date + ' ' + month + ' ' + year;
+    return time;
+  }
+  
+function addButton(cityname){
+      var addCity = document.createElement("button")
+          cityEl.appendChild(addCity)   
+          addCity.textContent = cityname;
+          addCity.setAttribute("class", "btn btn-success btn-lg")
+          
+          // addCity.addEventListener("click", )
+}
+
+function displayWeather(weather, container){
+  weatherDay = weatherWeek
+  cityname = nameInputEl.value.trim().toUpperCase();
+  temp = weatherDay.temp.day
+  wind = weatherDay.wind_speed
+  humidity =weatherDay.humidity
+  uvi = weatherDay.uvi
+  date = weatherDay.dt
+  date = timeConverter(date)
+  icon = weatherDay.weather[0].icon
+  console.log(icon);
+  icon = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
+  
+    
+    // weathrDay1 = weatherWeek[1]
+    
+    
+     
+
+
+
+
+  var weathers1 = document.createElement("button")
+  weathers1.setAttribute("class"," btn btn-success flex-row")
+  weatherContainerEl.appendChild(weathers1)
+  weathers1.textContent =  date ;
+
+  var weathers2 = document.createElement("img")
+  weathers2.setAttribute("src", icon)
+  weathers2.setAttribute("class", " flex-row")
+  weatherContainerEl.appendChild(weathers2)
+
+  var weathers3 = document.createElement("div")
+  weathers3.setAttribute("class","flex-row ")
+  weatherContainerEl.appendChild(weathers3)
+  weathers3.textContent ="Temp: " + temp + " °F";
+
+  var weathers4 = document.createElement("div")
+  weathers4.setAttribute("class","flex-row btn ")
+  weatherContainerEl.appendChild(weathers4)
+  weathers4.setAttribute("class","flex-row ")
+  weathers4.textContent ="Wind" + wind + "MPH";
+
+  var weathers5 = document.createElement("div")
+  weathers5.setAttribute("class","flex-row ")
+  weatherContainerEl.appendChild(weathers5)
+  weathers5.textContent ="Humidity: " + humidity + "%";
+  
+  var weathers6 = document.createElement("div")
+  weathers6.setAttribute("class","flex-row ")
+  weatherContainerEl.appendChild(weathers6)
+  weathers6.textContent ="UV index: " + uvi;
+}
+
+
+  userFormEl.addEventListener('submit', formSubmitHandler);
+  
